bf87674e82fcfa16aeb2ee8bb482e06d56b8bc23
mahipal <mahipalsingh92@gmail.com>
2023-01-13 13:40:38 +0530

    i change path of my image main,jgp

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ selectMenu = document.querySelectorAll("select"),
 
  ringtone = new Audio("./ringtone/alarm.mp3");
 
+//giving loops to hour min and units
+
 for(let i=12;i>0;i--){
     i= i<10 ? "0"+i :i;
     let option =`<option value ="${i}">${i}</option>`;
